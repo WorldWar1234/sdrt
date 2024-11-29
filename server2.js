45f70997eead0aad1cf8dd4a0286fedc5f199437
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-29 11:07:22 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,14 +1,35 @@
 "use strict";
 
+import cluster from "cluster";
+import os from "os";
 import express from "express";
 import compression from "compression";
 import helmet from "helmet";
 import proxy from "./proxy1.js";
-import cluster from "cluster";
-import os from "os";
 
-const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Clustering for multi-core systems
+if (cluster.isPrimary) {
+  const numClusters = process.env.CLUSTERS || (os.availableParallelism ? os.availableParallelism() : (os.cpus().length || 2));
+
+  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
+
+  // Fork workers
+  for (let i = 0; i < numClusters; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
+    cluster.fork();
+  });
+
+  return true;
+}
+
+// Worker processes run the Express server
+const app = express();
 
 // Use Helmet to secure your app
 app.use(helmet());
@@ -20,11 +41,12 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 // Middleware to handle favicon requests
 app.get("/favicon.ico", (req, res) => {
   res.status(204).end();
 });
-
 
 // Use the proxy function to handle all other requests
 app.use((req, res) => {
@@ -37,21 +59,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-// Clustering for multi-core systems
-if (cluster.isMaster) {
-  const numCPUs = os.cpus().length;
-  console.log(`Master ${process.pid} is running`);
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  // Start the server
-  app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} started, Server is running on port ${PORT}`);
-  });
-}
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Worker ${process.pid} started, Server is running on port ${PORT}`);
+});
