ac15a9eb0060d982ced4af12a7c288f57e9b2438
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-30 23:42:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
 
 import express from "express";
 import proxy from "./proxy1.js";
+import UserAgent from 'user-agents';
 
 const app = express();
 
 // Uncomment the next line if you want to trust the proxy
 // app.enable("trust proxy");
 app.disable("x-powered-by");
+
+// Middleware to set a random User-Agent header
+app.use((req, res, next) => {
+  const userAgent = new UserAgent();
+  req.headers['User-Agent'] = userAgent.toString();
+  next();
+});
 
 app.get("/", proxy);
 app.get("/favicon.ico", (req, res) => res.status(204).end());
