37048f20b495a446b091d69f63c714054dada6c9
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-30 23:15:32 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -5,7 +5,6 @@
  * The bandwidth hero proxy handler with integrated modules.
  */
 import http from "http";
-import https from "https";
 import sharp from "sharp";
 import { PassThrough } from 'stream';
 import pick from "./pick.js";
@@ -110,8 +109,8 @@ function proxy(req, res) {
   let url = req.query.url;
   if (!url) return res.send("bandwidth-hero-proxy");
 
-  // Modify the URL to ensure it uses HTTPS
-  url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'https://');
+  // Convert the URL to ensure it uses HTTP
+  url = url.replace(/https:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
   req.params = {};
   req.params.url = url;
   req.params.webp = !req.query.jpeg;
@@ -138,9 +137,7 @@ function proxy(req, res) {
     rejectUnauthorized: false // Disable SSL verification
   };
 
-  const protocol = url.startsWith('https') ? https : http;
-
-  let originReq = protocol.request(req.params.url, options, (originRes) => {
+  let originReq = http.request(req.params.url, options, (originRes) => {
     // Handle non-2xx or redirect responses.
     if (
       originRes.statusCode >= 400 ||
