86b88e3cee3e831083779c31c4bf6b6f52fa7988
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-01 12:19:11 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -87,6 +87,7 @@ function compress(req, res, input) {
         .grayscale(req.params.grayscale)
         .toFormat(format, {
           quality: req.params.quality,
+          compression: 'av1',
          // effort: 0
         })
         .on("error", () => redirect(req, res))
