0506955a567803811f129e6c22ac55c9ea0bc9d7
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-02 16:17:44 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -153,7 +153,7 @@ function hhproxy(req, res) {
 const requestModule = parsedUrl.protocol === 'https:' ? https : http;
 
   try {
-    let originReq = requestModule.request(parsedUrl, options, (originRes) => {
+    const originReq = requestModule.request(parsedUrl, options, (originRes) => {
       // Handle non-2xx or redirect responses.
       if (
         originRes.statusCode >= 400 ||
