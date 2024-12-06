bc548077d65767dcc25d838de164dfccdc07ebc5
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-06 15:56:13 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -71,7 +71,6 @@ function redirect(req, res) {
  * @param {http.IncomingMessage} input - The input stream for image data.
  */
 function compress(req, res, input) {
-function compress(req, res, input) {
   // Disable caching, SIMD, and set concurrency to 1
   sharp.cache(false);
   sharp.simd(false);
