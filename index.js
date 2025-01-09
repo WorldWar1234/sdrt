208b9836e65a64914e567ba44601f66504074363
Rushs321 <fyggh566@tutamail.com>
2025-01-09 13:37:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,8 @@
 
 import express from "express";
 import {fetchImageAndHandle} from "./proxy4.js";
-
 const app = express();
-
-
-app.set('trust proxy', false);
 app.disable("x-powered-by");
-
 app.get("/", fetchImageAndHandle);
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
