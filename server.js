5209471a578e4e8c73f7950645e2438137d31608
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-03 22:30:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import app from "./index.js";
-import http from "http";
+import http2 from "http2";
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer(app);
+const server = http2.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
