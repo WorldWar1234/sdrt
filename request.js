d19266055d45a0db4dd0f5c70028a8c0642ef923
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-11 21:06:36 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import sharp from 'sharp';
+import { Transform } from 'stream';
 
 // Constants
 const MIN_COMPRESS_LENGTH = 1024;
@@ -63,7 +64,7 @@ export async function fetchImageAndHandle(req, res) {
 
   try {
     const response = await axios.get(req.params.url, {
-      responseType: 'arraybuffer', // Get the response as an array buffer
+      responseType: 'stream',
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
@@ -90,15 +91,24 @@ export async function fetchImageAndHandle(req, res) {
       return res.status(response.status).send('Failed to fetch the image.');
     }
 
-    const buffer = Buffer.from(response.data, 'binary'); // Convert response data to buffer
+    const chunks = [];
+    response.data.on('data', chunk => chunks.push(chunk));
+    response.data.on('end', async () => {
+      const buffer = Buffer.concat(chunks);
 
-    if (shouldCompress(req)) {
-      await compress(req, res, buffer);
-    } else {
-      res.setHeader('Content-Type', req.params.originType);
-      res.setHeader('Content-Length', req.params.originSize);
-      res.end(buffer);
-    }
+      if (shouldCompress(req)) {
+        await compress(req, res, buffer);
+      } else {
+        res.setHeader('Content-Type', req.params.originType);
+        res.setHeader('Content-Length', req.params.originSize);
+        res.end(buffer);
+      }
+    });
+
+    response.data.on('error', (err) => {
+      console.error('Error receiving image data:', err.message);
+      res.status(500).send('Failed to fetch the image.');
+    });
   } catch (error) {
     console.error('Error fetching image:', error.message);
     res.status(500).send('Failed to fetch the image.');
