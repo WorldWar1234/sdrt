5c3abadf1423bb1f04575e00a5d845ee795f4ca3
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-12 12:19:26 +0530

    Update request.js

--- request.js
+++ request.js
@@ -27,6 +27,7 @@ async function compress(req, res, inputBuffer) {
     if (metadata.height > MAX_HEIGHT) {
       sharpInstance.resize({ height: MAX_HEIGHT });
     }
+    sharpInstance.sharpen(0.5);
 
     if (req.params.grayscale) {
       sharpInstance.grayscale();
