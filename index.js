10afa08c4f77ce96f2d781e1f9b1b5c8cabdc43c
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-22 19:50:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 "use strict";
 
 import express from "express";
-import {fetchImageAndHandle} from "./request.js";
+import {imageHandler} from "./request.js";
 const app = express();
 app.disable("x-powered-by");
-app.get("/", fetchImageAndHandle);
+app.get("/", imageHandler);
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 export default app;
