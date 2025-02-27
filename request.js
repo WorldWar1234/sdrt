e4d45b9d17b9fffa1fd98533dbcb6c00ab51aba9
WorldWar1234 <dsfsdfsgvd@proton.me>
2025-02-27 22:11:29 +0530

    Update request.js

--- request.js
+++ request.js
@@ -11,19 +11,11 @@ const MAX_HEIGHT = 16383; // Resize if height exceeds this value
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
 
-  if (!originType.startsWith('image')) return false;
-  if (originSize === 0) return false;
-  if (req.headers.range) return false;
-  if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
-  if (
-    !webp &&
-    (originType.endsWith('png') || originType.endsWith('gif')) &&
-    originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-  ) {
-    return false;
-  }
-
-  return true;
+  return originType.startsWith('image') &&
+         originSize > 0 &&
+         !req.headers.range &&
+         !(webp && originSize < MIN_COMPRESS_LENGTH) &&
+         !(!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH);
 }
 
 // Function to compress an image stream directly
@@ -56,8 +48,6 @@ async function compress(req, res, inputStream) {
   // Pipe the input stream to the transform stream
   inputStream.pipe(transformStream).pipe(res);
 
-  
-
   // Handle any errors from the input stream
   inputStream.on('error', (err) => {
     console.error('Error reading input stream:', err.message);
@@ -65,14 +55,13 @@ async function compress(req, res, inputStream) {
   });
 }
 
-
-
 // Function to handle image compression requests
 export async function fetchImageAndHandle(req, res) {
   const url = req.query.url;
   if (!url) {
     return res.status(400).send('Image URL is required.');
   }
+
   req.params = {
     url: decodeURIComponent(url),
     webp: !req.query.jpeg,
@@ -109,4 +98,4 @@ export async function fetchImageAndHandle(req, res) {
     console.error('Error fetching image:', error.message);
     res.status(500).send('Failed to fetch the image.');
   }
-                           }
+      }
