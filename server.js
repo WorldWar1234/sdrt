dc0072502409f9bb8f358ec5e7babd01839345e3
WorldWar1234 <dsfsdfsgvd@proton.me>
2025-03-01 23:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fastify.get('/', async (req, reply) => {
 // Start the server
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 8080 });
     fastify.log.info(`Server is running on port 3000`);
   } catch (err) {
     fastify.log.error(err);
