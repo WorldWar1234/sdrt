9c4cf51e343f157986bc119089351ad75b32f416
WorldWar1234 <dsfsdfsgvd@proton.me>
2025-03-01 23:19:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import Fastify from 'fastify';
 import { fetchImageAndHandle } from './request.js'; // Adjust the path as needed
 
-const fastify = Fastify({ logger: false });
+const fastify = Fastify({ logger: true });
 
 // Route to handle image compression requests
 fastify.get('/', async (req, reply) => {
@@ -11,8 +11,8 @@ fastify.get('/', async (req, reply) => {
 // Start the server
 const start = async () => {
   try {
-    await fastify.listen({ host: '0.0.0.0', port: 8080 });
-    fastify.log.info(`Server is running on port 8080`);
+    await fastify.listen({ port: 8080, host: '0.0.0.0' });
+    fastify.log.info(`Server is running on http://0.0.0.0:8080`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
